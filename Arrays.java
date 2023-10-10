bab32fef464768906d524cbc5a8db6ba5dbb0956
frost <frostabbi3@gmail.com>
2023-10-11 00:04:35 +0530

    Intellij Java Codes

--- Arrays.java
+++ Arrays.java
@@ -1,20 +1,5 @@
 import java.util.*;
 
-                          //(Simple Array Program)
-//public class Arrays {
-//    public static void main(String[] args){
-//
-//        Scanner sc=new Scanner(System.in);
-//        System.out.println("Enter the size:");
-//        int size=sc.nextInt();
-//
-//        int[] array=new int[size];
-//
-//        for(int i=0;i<size;i++){
-//            System.out.println(i);
-//        }
-//    }
-//}
 
                            //Program-1 (Minimum size of Arrays)
 
@@ -45,12 +30,12 @@ import java.util.*;
 //        int[] array2 = new int[size];
 //
 //
-//        System.out.println("Enter the elements of First Array:");
+//        System.out.println("Enter the elements of First Arrays:");
 //        for (i = 0; i < size; i++) {
 //            array1[i] = sc.nextInt();
 //        }
 //
-//        System.out.println("Enter the elements of Second Array:");
+//        System.out.println("Enter the elements of Second Arrays:");
 //        for (i = 0; i < size; i++) {
 //            array2[i] = sc.nextInt();
 //        }
@@ -130,7 +115,7 @@ import java.util.*;
 //    }
 //}
 
-                               //Program 3(find largest element in an array)
+                               //Program-3(find Largest element in an array)
 
 //class Arrays {
 //    public static void main(String[] args){
@@ -151,38 +136,167 @@ import java.util.*;
 //                max=array[i];
 //            }
 //        }
-//        System.out.println("Largest element of Array is: " + max);
+//        System.out.println("Largest element of Arrays is: " + max);
 //    }
 //}
 
-                              //Program 4(Array Program to Print a 2D Array)
+                                 //Program-4(Program to Find the Transpose)
+
+//class Arrays{
+//    public static void main(String[] args){
+//
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("Enter the size:");
+//        int size=sc.nextInt();
+//
+//        int[][] matrix=new int[size][size];
+//
+//        System.out.println("Enter the elements:");
+//
+//        for(int i=0;i<size;i++){
+//            for(int j=0;j<size;j++){
+//                matrix[i][j]=sc.nextInt();
+//            }
+//            System.out.println();
+//        }
+//
+//        System.out.println("Printing the elements:");
+//
+//        for(int i=0;i<size;i++){
+//            for(int j=0;j<size;j++){
+//                System.out.print(matrix[j][i] + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+                             //Program-5(Copy All the Elements of One Arrays to Another Arrays)
+
+//class Arrays{
+//    public static void main(String[] args){
+//
+//        Scanner sc=new Scanner(System.in);
+//        int size=sc.nextInt();
+//
+//        int[] a=new int[size];
+//        int[] b=new int[a.length];
+//
+//        for(int i=0;i<size;i++){
+//            a[i]=sc.nextInt();
+//        }
+//
+//        b=a;
+//
+//        //Printing
+//        for(int i=0;i<size;i++){
+//            System.out.print(a[i] + ",");
+//        }
+//
+//        System.out.println();
+//
+//        for(int i=0;i<a.length;i++) {
+//            System.out.print(b[i] + " ");
+//        }
+//    }
+//}
+
+                            //Program-6(Program to Check Whether Two Matrix Are Equal or Not)
+
+//class Arrays{
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter the number of rows for the matrices: ");
+//        int rows = sc.nextInt();
+//
+//        System.out.print("Enter the number of columns for the matrices: ");
+//        int columns = sc.nextInt();
+//
+//        int[][] matrix1 = new int[rows][columns];
+//        int[][] matrix2 = new int[rows][columns];
+//
+//        System.out.println("Enter the elements of the first matrix:");
+//
+//        // Input elements for the first matrix
+//        for (int i = 0; i < rows; i++) {
+//            for (int j = 0; j < columns; j++) {
+//                matrix1[i][j] = sc.nextInt();
+//            }
+//        }
+//
+//        System.out.println("Enter the elements of the second matrix:");
+//
+//        // Input elements for the second matrix
+//        for (int i = 0; i < rows; i++) {
+//            for (int j = 0; j < columns; j++) {
+//                matrix2[i][j] = sc.nextInt();
+//            }
+//        }
+//
+//        // Compare matrices for equality
+//        boolean isEqual = true;
+//        for (int i = 0; i < rows; i++) {
+//            for (int j = 0; j < columns; j++) {
+//                if (matrix1[i][j] != matrix2[i][j]) {              //Comparing two Matrix
+//                    isEqual = false;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        if (isEqual) {
+//            System.out.println("The two matrices are equal.");
+//        } else {
+//            System.out.println("The two matrices are not equal.");
+//        }
+//    }
+//}
+
+                                  //Program-7 (Compute the Sum of Diagonals of a Matrix)
 
 class Arrays{
     public static void main(String[] args){
 
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the size:");
-        int size=sc.nextInt();
+        System.out.print("Enter the size of Row:");
+        int row=sc.nextInt();
+        System.out.print("Enter the size of Column:");
+        int col=sc.nextInt();
 
-        int[][] matrix=new int[size][size];
+        int[][] matrix=new int[row][col];
 
-        System.out.println("Enter the elements:");
-
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 matrix[i][j]=sc.nextInt();
             }
-            System.out.println();
+
         }
 
-        System.out.println("Printing the elements:");
-
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
+        //Sum of Diagonal
+       ;
+        int Pri_Diagonal=0;
+        for(int i=0;i<row;i++){
+            Pri_Diagonal+=matrix[i][i];
         }
 
+        int Sec_Diagonal=0;
+        for(int i=0;i<row;i++){
+            Sec_Diagonal+=matrix[row-i-1][i];
+        }
+
+        //Printing the Sum of Diagonal
+
+        System.out.print("Sum of Primary Diagonal:");
+        System.out.println(Pri_Diagonal);
+        System.out.print("Sum of Secondary Diagonal:");
+        System.out.println(Sec_Diagonal);
+
+        int Result=Pri_Diagonal+Sec_Diagonal;
+
+        System.out.print("Total Result:");
+        System.out.println(Result);
     }
 }
+
+
+
